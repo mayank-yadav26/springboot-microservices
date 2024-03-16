670fac344ec850bc7ba6fbf8b39c76dc565f27cf
mayank-yadav <mayankyadav1503@gmail.com>
2024-03-16 16:26:55 +0530

    removing spring security

--- discovery-server/src/main/java/config/SecurityConfig.java
+++ discovery-server/src/main/java/config/SecurityConfig.java
@@ -1,18 +1,32 @@
-package config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
-
-	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
-		authenticationManagerBuilder.inMemoryAuthentication()
-		.withUser("eureka").password("password")
-		.authorities("USER");
-	}
-
-}
+//package config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+//		authenticationManagerBuilder.inMemoryAuthentication()
+//		.passwordEncoder(NoOpPasswordEncoder.getInstance())
+//		.withUser("eureka").password("password")
+//		.authorities("USER");
+//	}
+//	
+//	@Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		 http
+//         .authorizeHttpRequests((authz) -> authz
+//             .anyRequest().authenticated()
+//         )
+//         .httpBasic();
+//        return http.build();
+//    }
+//
+//}
