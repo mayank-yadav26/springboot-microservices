5fc19cab92ec81f8f07cbce65aca2ebc35c05b97
mayank-yadav <mayankyadav1503@gmail.com>
2024-03-17 20:09:51 +0530

    Adding circuit breaker

--- order-service/src/main/java/com/mayanktech/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/mayanktech/orderservice/controller/OrderController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mayanktech.orderservice.dto.OrderRequest;
 import com.mayanktech.orderservice.service.OrderService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,8 +22,15 @@ public class OrderController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@CircuitBreaker(name="inventory" , fallbackMethod = "fallbackMethod")
 	public String placeOrder(@RequestBody OrderRequest orderRequest) {
 		orderService.placeOrder(orderRequest);
 		return "Oder placed successfully";
 	}
+	
+	public String fallbackMethod(OrderRequest orderRequest , RuntimeException runtimeException) {
+		return "Oops! Something went wrong please order after some time!";
+	}
+	
+	
 }
